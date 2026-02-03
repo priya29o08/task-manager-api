65c6bc11a998981d9308a38f2fee73203772a82a
priya29o08 <priyadarshinijs29@gmail.com>
2026-02-03 23:34:52 +0530

    created html file with script and connected backend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify,request
+from flask_cors import CORS
 import sqlite3
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 #setting up database
 def init_db():
